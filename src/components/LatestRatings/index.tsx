3c1c6a1c74abe72966a348ba10cd9920ae103c6d
Júlio Cézar <captaingamergroot@gmail.com>
2023-05-10 10:26:39 -0300

    ✨ Começando a criação do RatingCard

--- src/components/LatestRatings/index.tsx
+++ src/components/LatestRatings/index.tsx
@@ -1,4 +1,5 @@
 import { ChartLineUp } from '@phosphor-icons/react'
+import { RatingCard } from '../RatingCard'
 import { Text } from '../Typograph'
 import { PageTitle } from '../ui/PageTitle'
 import { Container } from './styles'
@@ -14,7 +15,33 @@ export const LatestRatings = () => {
 
       <Text size="sm">Avaliações mais recentes</Text>
 
-      <section></section>
+      <section>
+        {Array.from({ length: 20 }).map((_, i) => (
+          <RatingCard
+            key={i}
+            rating={{
+              id: 'aa',
+              rate: 4,
+              user: {
+                name: 'Júlio',
+                avatar_url: '',
+                email: 'jczrpereira100',
+                id: 'kdjd',
+                created_at: new Date(),
+              },
+              book: {
+                author: 'dsgsd',
+                cover_url: '',
+                id: 'fvgdf',
+                name: 'fbd',
+                summary: 'fdsv',
+                total_pages: 189,
+              },
+              created_at: new Date(),
+            }}
+          />
+        ))}
+      </section>
     </Container>
   )
 }
---
+++ src/components/RatingCard/index.tsx
@@ -0,0 +1,37 @@
+import { getRelativeTimeString } from '@/utils/getRelativeTimeString'
+import { Book, Rating, User } from '@prisma/client'
+import Link from 'next/link'
+import { Text } from '../Typograph'
+import { Avatar } from '../ui/Avatar'
+import { Container, UserDetails } from './styles'
+
+export type RatingWithAuthorAndBook = Rating & {
+  user: User
+  book: Book
+}
+
+type RatingCardProps = {
+  rating: RatingWithAuthorAndBook
+}
+
+export const RatingCard = ({ rating }: RatingCardProps) => {
+  const distance = getRelativeTimeString(new Date(rating.created_at), 'pt-BR')
+  return (
+    <Container>
+      <UserDetails>
+        <section>
+          <Link href={`/profile/${rating.user_id}`}>
+            <Avatar src={rating.user.avatar_url!} alt={rating.user.name} />
+          </Link>
+          <div>
+            <Text>{rating.user.name}</Text>
+            <Text size="sm" color="gray-400">
+              {distance}
+            </Text>
+          </div>
+        </section>
+        nota
+      </UserDetails>
+    </Container>
+  )
+}
